ee9cdeb2a7473bbe370eb5456822fe40b0450ea1
Oscar <oscarrc.web@gmail.com>
2021-05-27 12:12:18 +0200

    [MOD] Test if contract deployed correctly and its address exists

--- test/NFToken.test.js
+++ test/NFToken.test.js
@@ -11,7 +11,10 @@ contract("NFToken", (accounts) => {
         it('deploys succesfully', async () => {
             contract = await NFToken.deployed();
             const address = contract.address;
-            assert.notEqual('');
+            assert.notEqual(address, '');
+            assert.notEqual(address, 0x0);
+            assert.notEqual(address, null);
+            assert.notEqual(address, undefined);
         })
     })
 })
