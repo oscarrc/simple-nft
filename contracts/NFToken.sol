61204355ef02b255905684d52cebbd5313645e38
Oscar <oscarrc.web@gmail.com>
2021-05-27 14:02:03 +0200

    [ADD] Basic token smart contract

--- contracts/NFToken.sol
+++ contracts/NFToken.sol
@@ -1,8 +1,23 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.8;
 
-import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
 
-contract NFToken is ERC721{
+contract NFToken is ERC721Enumerable{
+    string[] public tokens; //Array to track tokens
+    mapping(string => bool ) _tokenExists; //Map to check uniqueness of tokens
+
     constructor() ERC721("NFToken", "TOKEN") {}
+
+    function mint(string memory _token) public { // For production must use roles
+        // Require unique
+        require(!_tokenExists[_token]);
+        // Add token
+        tokens.push(_token);
+        uint _id = tokens.length;
+        // Call mint function
+        _mint(msg.sender, _id);
+        // Track token        
+        _tokenExists[_token] = true;
+    }
 }
--- test/NFToken.test.js
+++ test/NFToken.test.js
@@ -1,3 +1,5 @@
+const { assert } = require('chai');
+
 const NFToken = artifacts.require('../contracts/NFToken.sol');
 
 require('chai')
@@ -7,14 +9,63 @@ require('chai')
 contract("NFToken", (accounts) => {
     let contract;
 
-    describe('deplyment', async () => {
+    before(async () => {        
+        contract = await NFToken.deployed();
+    });
+
+    describe('deployment', async () => {
         it('deploys succesfully', async () => {
-            contract = await NFToken.deployed();
             const address = contract.address;
             assert.notEqual(address, '');
             assert.notEqual(address, 0x0);
             assert.notEqual(address, null);
             assert.notEqual(address, undefined);
+        })
+
+        it('has name', async () => {
+            const name = await contract.name();
+            assert.equal(name, 'NFToken')
+        })
+
+        it('has symbol', async () => {
+            const symbol = await contract.symbol();
+            assert.equal(symbol, 'TOKEN')
+        })
+    })
+
+    describe('minting', async () => {
+        it('creates new token', async () => {
+            const result = await contract.mint('token0');
+            const totalSupply = await contract.totalSupply();
+            const event = result.logs[0].args;
+            
+            //SUCCESS
+            assert.equal(totalSupply, 1);
+            assert.equal(event.tokenId.toNumber(), 1, 'id correct');
+            assert.equal(event.from, 0x0000000000000000000000000000000000000000, 'from correct');
+            assert.equal(event.to, accounts[0], 'to correct');
+
+            //FAILURE
+            await contract.mint('token0').should.be.rejected;
+        })
+    })
+
+    describe('indexing', async () => {
+        it('lists tokens', async () => {
+           let current;
+           let result = [];
+           let expected = [0,1,2,3].map(t => `token${t}`);
+
+           [1,2,3].forEach( async (t) =>  await contract.mint(`token${t}`));
+           const totalSupply = await contract.totalSupply();
+           
+
+           for(let i = 1; i <= totalSupply; i++){
+               current = await contract.tokens(i - 1);
+               result.push(current);
+           }
+
+           assert.equal(result.join(','), expected.join(','));
         })
     })
 })
